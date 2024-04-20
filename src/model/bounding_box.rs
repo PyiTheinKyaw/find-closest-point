cc281565c9c3cdbfb15f90fc5f76afb42915d65c
Ou Ou Yatlawkin <uutxeinkyaw@gmail.com>
2024-04-20 16:56:52 +0630

    calculate_sah_cost done

--- src/model/bounding_box.rs
+++ src/model/bounding_box.rs
@@ -1,9 +1,7 @@
-use std::process::id;
 use crate::functions::dataset::Dataset;
-use crate::model::point3d::Point3D;
 
 pub struct BoundingBox {
-    k: usize,
+    pub k: usize,
     pub min_coordinates: Vec<f32>,
     pub max_coordinates: Vec<f32>,
 }
@@ -17,7 +15,7 @@ impl BoundingBox {
     ///
     /// # Arguments
     ///
-    /// * `list` - A reference to a vector containing the points.
+    /// * `list` - A vector containing the ref of points.
     /// * `k` - The dimension of the points (number of coordinates).
     ///
     /// # Type Parameters
@@ -33,9 +31,12 @@ impl BoundingBox {
     /// ```
     /// use fnp::model::point3d::Point3D;
     /// use fnp::model::bounding_box::BoundingBox;
+    /// 
+    /// let pa = Point3D::new(1.0, 2.0, 3.0);
+    /// let pb = Point3D::new(4.0, 5.0, 6.0);
     ///
-    /// let points = vec![Point3D::new(1.0, 2.0, 3.0), Point3D::new(4.0, 5.0, 6.0)];
-    /// let bounding_box = BoundingBox::calculate_bounding_box(&points, 3);
+    /// let points = vec![&pa, &pb];
+    /// let bounding_box = BoundingBox::calculate_bounding_box(points, 3);
     /// ```
     ///
     /// # Panics
@@ -50,7 +51,7 @@ impl BoundingBox {
     /// If the list of points is empty, the resulting bounding box will have invalid coordinates.
     ///
     /// @author: Pyi Thein Kyaw
-    pub fn calculate_bounding_box<T>(list: &Vec<T>, k: usize) -> Self
+    pub fn calculate_bounding_box<T>(list: Vec<&T>, k: usize) -> Self
     where T: Dataset<T>
     {
         let mut min_coordinates: Vec<f32> = Vec::with_capacity(k);
@@ -84,42 +85,104 @@ impl BoundingBox {
         BoundingBox {k, min_coordinates, max_coordinates}
     }
 
-    fn calculate_surface_area(&self, k: usize) -> f32 {
+    /// Calculates the surface area of the bounding box.
+    ///
+    /// This method calculates the surface area of the bounding box represented by the `BoundingBox` instance.
+    /// It takes the dimension `k` as input, which specifies the number of dimensions of the bounding box.
+    ///
+    /// # Returns
+    ///
+    /// The surface area of the bounding box as a `f32`.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use fnp::model::bounding_box::BoundingBox; 
+    /// let bbox = BoundingBox {
+    ///     k: 3,
+    ///     min_coordinates: vec![0.0, 0.0, 0.0],
+    ///     max_coordinates: vec![1.0, 2.0, 3.0],
+    /// };
+    /// let surface_area = bbox.calculate_surface_area();
+    /// assert_eq!(surface_area, 22.0);
+    /// ```
+    ///
+    /// This example creates a `BoundingBox` instance with dimensions 1x2x3 and calculates its surface area.
+    /// The expected surface area for a cuboid with sides of length 1, 2, and 3 is 22.
+    ///
+    /// # Note
+    ///
+    /// This method calculates the surface area of the bounding box using the lengths of its sides along each axis.
+    /// It computes the length of each side by subtracting the minimum coordinate from the maximum coordinate
+    /// along each axis, then multiplies these lengths together and doubles the result to account for both
+    /// the top and bottom faces of the bounding box.
+    ///
+    /// # Panics
+    ///
+    /// This method may panic if the provided dimension `k` is greater than the length of either `min_coordinates`
+    /// or `max_coordinates`.
+    /// 
+    /// 
+    /// @author: Pyi Thein Kyaw
+    pub fn calculate_surface_area(&self) -> f32 {
 
         let mut axis_list = Vec::new();
-        let mut surface_area: f32;
+        let mut surface_area: f32 = 0.0;
 
-        for index in 0..k {
+        for index in 0..self.k {
             axis_list.push(&self.max_coordinates[index] - &self.min_coordinates[index]);
         }
 
-        let mut axis_iter = axis_list.iter();
+        for (index, elements) in axis_list.iter().enumerate() {
+            
+            let next_index = (index + 1)  % axis_list.len();
+            surface_area += elements * axis_list[next_index];
+        }
 
-        let value = axis_iter.next();
+        surface_area = surface_area * 2.0;
 
-        todo!()
-
+        surface_area        
     }
 }
 
 #[cfg(test)]
 mod tests {
-    use super::*;
+    
+    use crate::model::point3d::Point3D;
+    use crate::model::bounding_box::BoundingBox;
+
     #[test]
     fn test_calculate_bounding_box() {
         // Create a list of points for testing
         let points = vec![
-            Point3D { x: 1.0, y: 2.0, z: 3.0 },
-            Point3D { x: 2.0, y: 3.0, z: 4.0 },
-            Point3D { x: 3.0, y: 4.0, z: 5.0 },
-            Point3D { x: 4.0, y: 5.0, z: 6.0 },
+            &Point3D { x: 1.0, y: 2.0, z: 3.0 },
+            &Point3D { x: 2.0, y: 3.0, z: 4.0 },
+            &Point3D { x: 3.0, y: 4.0, z: 5.0 },
+            &Point3D { x: 4.0, y: 5.0, z: 6.0 },
         ];
 
         // Call the function to calculate the bounding box
-        let bounding_box = BoundingBox::calculate_bounding_box(&points, 3);
+        let bounding_box = BoundingBox::calculate_bounding_box(points, 3);
 
         // Assert that the minimum and maximum coordinates are correct
         assert_eq!(bounding_box.min_coordinates, vec![1.0, 2.0, 3.0]);
         assert_eq!(bounding_box.max_coordinates, vec![4.0, 5.0, 6.0]);
+    }
+
+    #[test]
+    fn test_calculate_surface_area() {
+        // Create a BoundingBox instance for testing
+        let bbox = BoundingBox {
+            k: 3,
+            min_coordinates: vec![0.0, 0.0, 0.0],
+            max_coordinates: vec![1.0, 2.0, 3.0],
+        };
+
+        // Call the function to calculate the surface area
+        let surface_area = bbox.calculate_surface_area();
+
+        // Assert that the calculated surface area is correct
+        // The expected surface area for a cuboid with sides of length 1, 2, and 3 is 22
+        assert_eq!(surface_area, 22.0);
     }
 }
--- src/model/sah.rs
+++ src/model/sah.rs
@@ -2,6 +2,7 @@ use std::cell::RefCell;
 use std::rc::Rc;
 use crate::functions::dataset::Dataset;
 use crate::functions::sortable::Sortable;
+use crate::model::bounding_box::BoundingBox;
 use crate::model::kd_tree::KDTree;
 use crate::model::point3d::Point3D;
 
@@ -17,8 +18,75 @@ impl SAH
     {
         todo!()
     }
-    fn calculate_sah_cost<T>(sorted_list: &Vec<T>, k: usize, split_value: usize) -> f32 {
-        todo!()
+
+    /// Calculates the Surface Area Heuristic (SAH) cost for splitting a dataset along a given axis.
+    ///
+    /// This function computes the SAH cost for splitting a dataset represented by `sorted_list` along
+    /// the specified `axis` at the given `split_value`. The SAH cost is calculated as twice the sum
+    /// of the surface areas of the bounding boxes of the two subsets resulting from the split.
+    ///
+    /// # Arguments
+    ///
+    /// * `sorted_list` - A sorted list of elements representing the dataset.
+    /// * `axis` - The axis along which to split the dataset (e.g., 0 for X-axis, 1 for Y-axis).
+    /// * `k` - The dimension of the elements in the dataset.
+    /// * `split_value` - The value used to split the dataset along the specified axis.
+    ///
+    /// # Type Parameters
+    ///
+    /// * `T` - The type of the elements in the dataset. Must implement the `Dataset<T>` trait.
+    ///
+    /// # Returns
+    ///
+    /// The SAH cost for the split as a `f32` value.
+    ///
+    /// # Note
+    ///
+    /// This function first partitions the dataset into two subsets based on the `split_value` and `axis`.
+    /// It then calculates the bounding boxes for the left and right subsets using the `calculate_bounding_box`
+    /// method of the `BoundingBox` struct. Next, it computes the surface areas of the bounding boxes using
+    /// the `calculate_surface_area` method. Finally, it returns twice the sum of the surface areas of the
+    /// bounding boxes of the left and right subsets as the SAH cost for the split.
+    ///
+    /// # Example
+    ///
+    /// ```
+    /// use fnp::model::point3d::Point3D;
+    /// use fnp::model::sah::SAH;;
+    /// 
+    /// let sorted_list = vec![
+    ///     Point3D::new(1.0, 2.0, 3.0),
+    ///     Point3D::new(4.0, 5.0, 6.0),
+    ///     Point3D::new(7.0, 8.0, 9.0),
+    /// ];
+    ///
+    /// // Calculate the SAH cost for splitting along the X-axis at split value 4.0
+    /// let sah_cost = SAH::calculate_sah_cost(&sorted_list, 0, 3, 4.0);
+    /// ```
+    ///
+    /// This example calculates the SAH cost for splitting a dataset of 3D points along the X-axis at a split value of 4.0.
+    /// The expected SAH cost will depend on the specific dataset and splitting criteria.
+    /// 
+    /// @author: Pyi Thein Kyaw
+    pub fn calculate_sah_cost<T>(
+        sorted_list: &Vec<T>,
+        axis: usize,
+        k: usize,
+        split_value: f32
+    ) -> f32 
+    where T: Dataset<T>
+    {
+        // Partition dataset into two subsets baed on split_value and axis of each dimension (x,y,z, etc..)
+        let (left_subset, right_subset): (Vec<&T>, Vec<&T>) = Self::partition_dataset(sorted_list, split_value, axis);
+        let (left_size, right_size) = (left_subset.len(), right_subset.len());
+        
+        let left_bounding_box = BoundingBox::calculate_bounding_box(left_subset, k);
+        let right_bounding_box = BoundingBox::calculate_bounding_box(right_subset, k);
+
+        let surface_area_left = left_bounding_box.calculate_surface_area();
+        let surface_area_right = right_bounding_box.calculate_surface_area();
+
+        2.0 * ((left_size as f32 * surface_area_left) + (right_size as f32 * surface_area_right))
     }
 
     /// Partitions a dataset into two subsets based on a split value along a specified axis.
@@ -32,7 +100,7 @@ impl SAH
     ///
     /// # Arguments
     ///
-    /// * `values` - A vector containing the dataset to be partitioned.
+    /// * `values` - A ref of vector containing the dataset to be partitioned.
     /// * `split_value` - The value used to partition the dataset along the specified axis.
     /// * `axis` - The index of the axis along which to perform the partitioning.
     ///
@@ -42,7 +110,7 @@ impl SAH
     ///
     /// # Returns
     ///
-    /// A tuple `(left_subset, right_subset)` containing the left and right subsets of the dataset
+    /// A tuple `(left_subset, right_subset)` containing the left and right subsets of the dataset by ref
     /// after partitioning.
     ///
     /// # Note
@@ -58,7 +126,7 @@ impl SAH
     /// use fnp::model::sah::SAH;
     ///
     /// let points = vec![Point3D::new(1.0, 2.0, 3.0), Point3D::new(4.0, 5.0, 6.0)];
-    /// let (left_subset, right_subset) = SAH::partition_dataset(points, 2.5, 0);
+    /// let (left_subset, right_subset) = SAH::partition_dataset(&points, 2.5, 0);
     /// ```
     ///
     /// This example partitions a dataset of 3D points along the X-axis with a split value of `2.5`.
@@ -67,23 +135,22 @@ impl SAH
     ///
     ///
     /// @author: Pyi Thein Kyaw
-    pub fn partition_dataset<T>(
-        values: Vec<T>,
+    pub fn partition_dataset<'p,T>(
+        values: &'p Vec<T>,
         split_value: f32,
         axis: usize
-    ) -> (Vec<T>, Vec<T>)
+    ) -> (Vec<&'p T>, Vec<&'p T>)
     where T: Dataset<T>
     {
 
-        let mut left_subset: Vec<T> = vec![];
-        let mut right_subset: Vec<T> = vec![];
+        let mut left_subset: Vec<&T> = vec![];
+        let mut right_subset: Vec<&T> = vec![];
 
-        for point in values.into_iter() {
+        for point in values.iter() {
 
-            let mut value: &f32;
             let point_coord = point.get_internal_state();
 
-            value = &point_coord[axis];
+            let value = &point_coord[axis];
 
             // Check the coordinate value along the specified dimension
             if value < &split_value {
@@ -115,37 +182,54 @@ mod tests {
         ];
 
         // Split the dataset based on x dimension with split value of 2.5
-        let (left, right) = SAH::partition_dataset(points.clone(), 2.5, 0);
+        let (left, right) = SAH::partition_dataset(&points, 2.5, 0);
 
         // Ensure correct partitioning
         assert_eq!(left.len(), 2);
         assert_eq!(right.len(), 2);
-        assert!(left.contains(&Point3D { x: 1.0, y: 2.0, z: 3.0 }));
-        assert!(left.contains(&Point3D { x: 2.0, y: 3.0, z: 4.0 }));
-        assert!(right.contains(&Point3D { x: 3.0, y: 4.0, z: 5.0 }));
-        assert!(right.contains(&Point3D { x: 4.0, y: 5.0, z: 6.0 }));
+        assert!(left.contains(&&Point3D { x: 1.0, y: 2.0, z: 3.0 }));
+        assert!(left.contains(&&Point3D { x: 2.0, y: 3.0, z: 4.0 }));
+        assert!(right.contains(&&Point3D { x: 3.0, y: 4.0, z: 5.0 }));
+        assert!(right.contains(&&Point3D { x: 4.0, y: 5.0, z: 6.0 }));
 
         // Split the dataset based on y dimension with split value of 3.5
-        let (left, right) = SAH::partition_dataset(points.clone(), 3.5, 1);
+        let (left, right) = SAH::partition_dataset(&points, 3.5, 1);
 
         // Ensure correct partitioning
         assert_eq!(left.len(), 2);
         assert_eq!(right.len(), 2);
-        assert!(left.contains(&Point3D { x: 1.0, y: 2.0, z: 3.0 }));
-        assert!(left.contains(&Point3D { x: 2.0, y: 3.0, z: 4.0 }));
-        assert!(right.contains(&Point3D { x: 3.0, y: 4.0, z: 5.0 }));
-        assert!(right.contains(&Point3D { x: 4.0, y: 5.0, z: 6.0 }));
+        assert!(left.contains(&&Point3D { x: 1.0, y: 2.0, z: 3.0 }));
+        assert!(left.contains(&&Point3D { x: 2.0, y: 3.0, z: 4.0 }));
+        assert!(right.contains(&&Point3D { x: 3.0, y: 4.0, z: 5.0 }));
+        assert!(right.contains(&&Point3D { x: 4.0, y: 5.0, z: 6.0 }));
 
         // Split the dataset based on z dimension with split value of 4.5
-        let (left, right) = SAH::partition_dataset(points.clone(), 4.5, 2);
+        let (left, right) = SAH::partition_dataset(&points, 4.5, 2);
 
         // Ensure correct partitioning
         assert_eq!(left.len(), 2);
         assert_eq!(right.len(), 2);
-        assert!(left.contains(&Point3D { x: 1.0, y: 2.0, z: 3.0 }));
-        assert!(left.contains(&Point3D { x: 2.0, y: 3.0, z: 4.0 }));
-        assert!(right.contains(&Point3D { x: 3.0, y: 4.0, z: 5.0 }));
-        assert!(right.contains(&Point3D { x: 4.0, y: 5.0, z: 6.0 }));
+        assert!(left.contains(&&Point3D { x: 1.0, y: 2.0, z: 3.0 }));
+        assert!(left.contains(&&Point3D { x: 2.0, y: 3.0, z: 4.0 }));
+        assert!(right.contains(&&Point3D { x: 3.0, y: 4.0, z: 5.0 }));
+        assert!(right.contains(&&Point3D { x: 4.0, y: 5.0, z: 6.0 }));
+    }
+
+    #[test]
+    fn test_calculate_sah_cost() {
+        // Create a sorted list of points for testing
+        let sorted_list = vec![
+            Point3D::new(1.0, 2.0, 3.0),
+            Point3D::new(4.0, 5.0, 6.0),
+            Point3D::new(7.0, 8.0, 9.0),
+        ];
+
+        // Calculate the SAH cost for splitting along the X-axis at split value 4.0
+        let sah_cost = SAH::calculate_sah_cost(&sorted_list, 0, 3, 4.0);
+
+        // Assert that the calculated SAH cost matches the expected value
+        // The expected value can be calculated based on the surface areas of the bounding boxes
+        assert_eq!(sah_cost, 216.0); 
     }
 }
 
